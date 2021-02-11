8c9f3c817b25868c9249978798b08b3527ed8d7b
Lakshya Sharma <lakshyatu@gmail.com>
2021-02-11 21:33:11 +0530

    Added Question1
    Tell if a string can be constructed by given list of string. You can repeat, rearrange strings or not use some of it.

---
+++ src/main/java/recursion/Question1.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.util.List;
+
+/**
+ * Tell if a string can be constructed by given list of strings.
+ * You can repeat, rearrange strings or not use some of it.
+ *
+ * Sample Test Case:
+ * List of string: "a","aba","c","d"
+ * Create: cacaabaaba
+ * Output: true
+ */
+public class Question1 {
+
+    public boolean solve(List<String> a, String b) {
+        boolean result = false;
+        if (b.isEmpty()) {
+            return true;
+        }
+        for (int i = 1; i <= b.length(); i++) {
+            String sub = b.substring(0,i);
+            if (a.contains(sub)) {
+                result = solve(a, b.substring(i));
+            }
+        }
+        return result;
+    }
+}
+

0c07c44bf6f8a06771c7c789a59938344dec20bb
Lakshya Sharma <lakshyatu@gmail.com>
2021-02-13 12:16:28 +0530

    multiply number
    multiply number

---
+++ src/main/java/maths/Multiply.java
@@ -0,0 +1,16 @@
+package maths;
+
+/**
+ * Return multiplication of two integers
+ * <p>
+ * Sample Test Case:
+ * 4 2
+ * Output: 8
+ */
+public class Multiply {
+
+    public int solve(int a, int b) {
+        return a * b;
+    }
+}
+
